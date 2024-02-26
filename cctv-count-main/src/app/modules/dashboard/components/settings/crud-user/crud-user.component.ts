d7b2dbd6973717255f0c11e71d047d866b6f8b36
ronnapum <wroniso29110@gmail.com>
2024-02-26 21:39:52 +0700

    add HTTP method to interact with backend

--- cctv-count-main/src/app/modules/dashboard/components/settings/crud-user/crud-user.component.ts
+++ cctv-count-main/src/app/modules/dashboard/components/settings/crud-user/crud-user.component.ts
@@ -34,17 +34,39 @@ export class CrudUserComponent implements OnInit {
     this.selectedUser = { ...user };
     this.isNewUser = false;
   }
+
   addUser(): void {
     this.isNewUser = true;
     this.selectedUser = {}; // Clear selected user data
     this.showEditModal = true;
-    this.users.push({});
-  }
+    const maxId = this.users.reduce((max, user) => {
+      return Math.max(max, parseInt(user.id));
+    }, 0);
+  
+    // Create a dummy user locally
+    const dummyUser = {
+      id: (maxId + 1).toString(), 
+      username: 'newUser',
+      role: 'user',
+      password: '12345678'
+    };
 
+    this.users.push(dummyUser);
+    this.http.post('http://localhost:3000/users', this.selectedUser).subscribe(() => {
+        this.loadData();
+      });
+    this.selectUser(dummyUser);
+  }
+  
   saveUser(): void {
     if (this.isNewUser) {
-      this.users.push(this.selectedUser);
+      this.http.post('http://localhost:3000/users', this.selectedUser).subscribe(() => {
+        this.loadData();
+      });
     } else {
+      this.http.put(`http://localhost:3000/users/${this.selectedUser.id}`, this.selectedUser).subscribe(() => {
+        this.loadData();
+      });
     }
     this.showEditModal = false;
   }
@@ -54,9 +76,10 @@ export class CrudUserComponent implements OnInit {
     this.showEditModal = true;
   }
 
-
   saveChanges(editedUser: any): void {
-    // Logic to save changes for user goes here
+    this.http.put(`http://localhost:3000/users/${editedUser.id}`, editedUser).subscribe(() => {
+      this.loadData(); 
+    });
     this.showEditModal = false;
   }
 
@@ -64,21 +87,20 @@ export class CrudUserComponent implements OnInit {
     this.showEditModal = false;
   }
 
-  openModal() {
+  openModal(): void {
     this.showModal = true;
   }
 
-  closeModal() {
+  closeModal(): void {
     this.showModal = false;
   }
 
-  public deleteUser(user: any): void {
+  deleteUser(user: any): void {
     const confirmed = confirm("Are you sure you want to delete this user?");
     if (confirmed) {
-      const index = this.users.findIndex((u) => u.id === user.id);
-      if (index !== -1) {
-        this.users.splice(index, 1);
-      }
+      this.http.delete(`http://localhost:3000/users/${user.id}`).subscribe(() => {
+        this.loadData(); 
+      });
     }
   }
 }
